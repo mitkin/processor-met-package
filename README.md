a287c6f5c2f511113427d954cee39a90da396157
Mikhail Itkin <itkin.m@gmail.com>
2018-05-20 18:15:53 +0200

    Added ice charts, topaz forecast and Arome Arctic forecasts

---
+++ README.md
@@ -0,0 +1,4 @@
+# MET package processor
+
+Obtain atmospheric forecast, sea ice forecast and ice chart from MET Norway.
+Package the bundle as zip file for shipping.
--- met-package.py
+++ met-package.py
@@ -16,10 +16,54 @@ from urllib import request
 from zipfile import ZipFile
 import io
 from io import StringIO
-import requests
-import tempfile
 import ftplib
+import netCDF4 as nc
+import xarray as xr
 
+
+def get_topaz_forecast(output_dir):
+    baseurl = "http://thredds.met.no/thredds/dodsC/topaz/dataset-topaz4-arc-unmasked-be"
+    varnames = [
+            # "fice",
+            # "hice",
+            # "fy_age",
+            # "fy_frac",
+            # "hsnow",
+            "uice",
+            "vice",
+            "hice",
+            "fice",
+            ""
+            ]
+    tdim = 42
+    xdim = 608
+    ydim = 880
+    xmin = 280
+    xmax = 420
+    ymin = 240
+    ymax = 460
+
+    varlist = "[0:1:3][240:1:460][280:1:420],".format(tdim, ydim, xdim).join(varnames)
+    varlist += "time[0:1:{}]".format(tdim)
+    varlist += ",latitude[240:1:460][280:1:420]"
+    varlist += ",longitude[240:1:460][280:1:420]"
+
+    thredds_request = baseurl + "?" + varlist
+    xdst = xr.open_dataset(thredds_request)
+    comp = dict(zlib=True, complevel=9)
+    encoding = {var: comp for var in xdst.variables}
+
+    timestamp = str(xdst.time[0].data).split('T')[0]
+
+    outname = os.path.join(output_dir, 'topaz_{}.nc'.format(timestamp))
+    xdst.to_netcdf(outname, engine='netcdf4', encoding=encoding)
+
+def get_weather_forecast(output_dir):
+    forecast_url = "http://thredds.met.no/thredds/ncss/aromearcticlatest/arome_arctic_extracted_2_5km_latest.nc?var=air_temperature_2m&var=wind_direction&var=wind_speed&disableLLSubset=on&disableProjSubset=on&horizStride=6&time_start=2018-05-20T06%3A00%3A00Z&time_end=2018-05-25T00%3A00%3A00Z&timeStride=6&vertCoord=&addLatLon=true"
+    response = request.urlopen(forecast_url)
+    fname = os.path.basename("weather-forecast.nc")
+    with open(os.path.join(output_dir, fname), mode='wb') as f:
+                f.write(response.read())
 
 def get_ice_charts(output_dir, add_date=True):
     """
@@ -79,7 +123,7 @@ def main():
 
     args = p.parse_args()
 
-    logger = create_logger(args.log_file)
+    create_logger(args.log_file)
 
     output_dir = args.output_file + '.d'
     try:
@@ -87,10 +131,12 @@ def main():
     except:
         logger.debug("Directory {} already exists".format(output_dir))
 
+    # Obtain data
     get_ice_charts(output_dir)
+    get_weather_forecast(output_dir)
+    get_topaz_forecast(output_dir)
 
     shutil.make_archive(os.path.splitext(args.output_file)[0], 'zip', output_dir)
-
 
     if not args.keep_temporary:
          shutil.rmtree(output_dir)
